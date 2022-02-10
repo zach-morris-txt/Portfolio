229064a09ebd44e1a760f52017ea9ea223ab1c21
zach-morris-txt <zach-morris@lambdastudents.com>
2022-02-10 12:05:35 -0500

    Initial data, Initial routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
+import ProjectList from '../Components/ProjectList'
 
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <ProjectList />
         <Footer />
     </div>
   )
