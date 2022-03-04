7040d9446a7b8d675ed2c375d3630d864c1096e3
zach-morris-txt <zach-morris@lambdastudents.com>
2022-03-04 10:33:56 -0500

    Contact Form; EmailJS

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
+import '../Styles/Contact.css'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 
 
 const Contact = () => {
+    const form = useRef();
+    function sendEmail(e) {
+      e.preventDefault();
+
+      emailjs.sendForm('service_o41xzyd', 'template_c70amym', form.current, 'rRq4k2FhFObFtegju')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        form.current.reset()
+    }
+
+
   return (
     <div>
       <Navbar />
+      <div className='container'>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='form-group'>
+            <input type="text" id='subject-contact' name='subject' value='Contact' readOnly/>
+          </div>
+          <div className='form-group'>
+            <input type="text" id='name' name='name' placeholder='Name'></input>
+          </div>
+          <div className='form-group'>
+            <input type="email" id='email' name='email' placeholder='Email' required></input>
+          </div>
+          <div className='form-group'>
+            <textarea id='message' name='message' placeholder='Email Me' cols="30" rows="10" required></textarea>
+          </div>
+          <input type="submit" value='Send' readOnly/>
+        </form>
+      </div>
+      <div id='status' className='success'>Success</div>
+
       <div className=''>
-        
+        <div className=''>LinkedIn</div>
+        <div className=''>GitHub</div>
+        <div className=''>Email -professional-</div>
+        <div className=''>phone#???</div>
       </div>
       <Footer />
+      <script src="https://smtpjs.com/v3/smtp.js"></script>
     </div>
   )
 }
