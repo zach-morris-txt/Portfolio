44cb6e6513fd9bf49c201b3afc4d1f58a55bb098
zach-morris-txt <zach-morris@lambdastudents.com>
2022-03-08 16:18:33 -0500

    Styling Home; Data & Scroll

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -7,18 +7,18 @@ import ecommerceProject from '../Assets/Images/Ecommerce.PNG'
 export const projects = [
     {
         id: 0,
-        title: "Cali-Cafe Site",
+        title: "Cali-Cafe",
         coverImg: caliProject,
-        description1: "",
+        description1: "Website for a previously unlisted, local café.",
         techStack: "React | FontAwesome",
         deployedLink: "https://cali-site.herokuapp.com/",
         githubLink: "https://github.com/zach-morris-txt/cali-site",
     },
     {
         id: 1,
-        title: "User-Account Fullstack",
+        title: "Market User-Onboarding",
         coverImg: userAccountProject,
-        description1: "",
+        description1: "Fullstack app for a demo, fresh produce market. Features user signup, user login, and relational data management.",
         techStack: "React | Redux | PostgreSQL",
         deployedLink: "https://frontend-market.herokuapp.com/",
         githubLink: "https://github.com/zach-morris-txt/Refactor-Markets-Frontend",
@@ -28,7 +28,7 @@ export const projects = [
         id: 2,
         title: "Coderheroes E-Learning (Private)", //Provide Bug Checklist
         coverImg: coderheroProject,
-        description1: "",
+        description1: "Fullstack app for a startup education platform. Features okta account management, backend class registration, and stripe payment processing.",
         techStack: "React | Redux | Okta | Stripe | AntDesign",
         deployedLink: "https://a.coderheroes.dev/login",
         githubLink: "https://github.com/zach-morris-txt/coder-heroes-fe",
@@ -36,9 +36,9 @@ export const projects = [
     },
     {
         id: 3,
-        title: "Ecommerce Fullstack (Under Construction)",
+        title: "StyleRider Ecommerce (Under Construction)",
         coverImg: ecommerceProject,
-        description1: "",
+        description1: "Fullstack app for a demo, motorcylce-gear vendor. Features product filtering, full product CRUD, and a working customer cart.",
         techStack: "React | Redux | Express | MongoDB",
         deployedLink: "https://ecommerce-backend--api.herokuapp.com/",
         githubLink: "https://github.com/zach-morris-txt/EcommerceAPI",
