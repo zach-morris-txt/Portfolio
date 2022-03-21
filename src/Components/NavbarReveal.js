56bd60eaa633cb359e57d972906f721e2fa0409f
zach-morris-txt <zach-morris@lambdastudents.com>
2022-03-20 22:10:07 -0400

    Mobile Styling HomeSlide; Final

--- src/Components/NavbarReveal.js
+++ src/Components/NavbarReveal.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 import '../Styles/Navbar.css'
 import manillaFull from '../Assets/Images/IconIMGs/manillaFull.png'
-import résumé from '../Assets/Images/IconIMGs/0.png'
+import résumé from '../Assets/Images/IconIMGs/Résumé.png'
 import manillaSlide from '../Assets/Images/IconIMGs/manillaSlide.png'
 
 
