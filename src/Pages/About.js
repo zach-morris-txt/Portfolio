774d4f220f960d09770c5162a0da77e97a5113ae
zach-morris-txt <zach-morris@lambdastudents.com>
2022-03-26 11:39:25 -0400

    Tablet-Desktop AboutPage; Final

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -17,21 +17,21 @@ const About = () => {
       <Navbar />
       <div className='aboutContainer'>
         <div className='aboutText'>
-          <div className='aboutBold'>Tampa, FL- I am a JavaScript/ Node.js Developer.</div>
-          <p className='aboutRegular'>I hold the scandalous conviction that the peacock is better than the rhino,
-            though, I mostly hold this view because the rhino is twice as costly. I'll change my tune as soon as someone mails me a free copy</p>
+          <div className='aboutBold'><span className='aboutItalic'>Tampa, FL</span> - I am a JavaScript/ Node.js Developer.</div>
+          <p className='aboutRegular'>I hold the scandalous conviction that the peacock is better than the rhino. However, I hold this view strictly because the rhino is twice as costly.
+          If anyone would like to change my mind by mailing me a free copy you are more than welcome to do so, I embrace incorrect opinions.</p>
           <div className='aboutBooks'>
-            <img src={definitiveJS} className='jsBooks' alt='Definitive JavaScript Book' />
-            <img src={eloquentJS} className='jsBooks' alt='Eloquent JavaScript Book' />
+            <img src={definitiveJS} className='jsBooks' alt='Definitive JavaScript Book' loading="lazy" />
+            <img src={eloquentJS} className='jsBooks' alt='Eloquent JavaScript Book' loading="lazy" />
           </div>
           <p className='aboutRegular'>I've had a life-long passion for circuitry, code, and economics. There's something about unravelling a stubborn puzzle that really calls to me. Fortunately, 
             Computer Science is riddled with stubborn bugs. I have freelancer ambitions and I'd like to learn some software development; at least enough to be dangerous.</p>
           <div className='aboutRegular'><div className='aboutBold'>Interests:</div> Arduino, Homesteading, Make: Magazine, Motorcycles, The Rich Dad Podcast</div>
         </div>
-        <div className='aboutImgs'>
-          <button id='aboutButton blueHover' onClick={()=>setReveal(!reveal)}>{reveal? 'A Less Horrifying Photo' : 'Hacker Man'}</button>
+        <div id='aboutImgs' className={reveal? 'aboutImgs' : 'aboutImgsAlt'}>
+          <button id='aboutButton' className='blueHover' onClick={()=>setReveal(!reveal)}>{reveal? 'Never Click Me Again' : 'Click Me'}</button>
           <img src={about1} className='aboutImg' id='about1' alt='Zach Morris' />
-          <img src={about2} className='aboutImg' id={reveal? 'about2Show' : 'about2Hide'} alt='Zach Morris' />
+          <img src={about2} className='aboutImg' id={reveal? 'about2Show' : 'about2Hide'} alt='Zach Morris' loading="lazy"/>
         </div>
       </div>
       <Footer />
